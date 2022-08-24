6fbd087a5fab0dc79915ccdd7f34ade7658f4def
iso88591 <18179157787@163.com>
2022-08-24 15:39:29 +0800

    自定义layout

--- app/src/main/java/me/iso88591/cacu/MainActivity.kt
+++ app/src/main/java/me/iso88591/cacu/MainActivity.kt
@@ -1,18 +1,18 @@
 package me.iso88591.cacu
 
 import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -21,17 +21,55 @@ import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.platform.*
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.view.WindowCompat
+import me.iso88591.cacu.logics.CacuKeys
 //import me.iso88591.cacu.ui.cacu.SimpleCacu
 import me.iso88591.cacu.ui.theme.CacuTheme
 
 class MainActivity : ComponentActivity() {
+
+    private fun SimpleVerticalKeys() = buildGridItems {
+        listOf<CacuKeys>(
+            CacuKeys.C,
+            CacuKeys.PlusOrReduce,
+            CacuKeys.Percent,
+            CacuKeys.Div,
+
+            CacuKeys.Num(7),
+            CacuKeys.Num(8),
+            CacuKeys.Num(9),
+            CacuKeys.Mul,
+
+            CacuKeys.Num(4),
+            CacuKeys.Num(5),
+            CacuKeys.Num(6),
+            CacuKeys.Reduce,
+
+            CacuKeys.Num(1),
+            CacuKeys.Num(2),
+            CacuKeys.Num(3),
+            CacuKeys.Plus,
+
+            CacuKeys.Num(0,2),CacuKeys.Point, CacuKeys.Result
+        ).forEach {
+            item(it.spanCount) {
+                it.Show(modifier = Modifier)
+            }
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -52,12 +90,131 @@ class MainActivity : ComponentActivity() {
 ////                        screenText += it.key
 //                    }
 
-                    UseGrid()
+//                    UseGrid()
+
+                    UseLayout(4,
+                        10,
+                        10,
+                        Modifier
+                            .background(Color(0xff010101))
+                            .padding(horizontal = 10.dp),
+                        remember { SimpleVerticalKeys() }
+                    )
 
                 }
             }
         }
     }
+
+    private fun log(vararg args: Any?) {
+        val content = args.joinToString(separator = ":")
+        Log.e("TAG", "log: ===========${content}")
+    }
+
+    data class MyGridLayoutScopeModel(
+        val weight: Int,
+        val content: @Composable () -> Unit
+    )
+
+    fun interface GridLayoutScope {
+
+        fun item(weight: Int, content: @Composable () -> Unit)
+
+        fun item(content: @Composable () -> Unit) = item(1, content)
+
+    }
+
+    fun buildGridItems(
+        scope: GridLayoutScope.() -> Unit
+    ): List<MyGridLayoutScopeModel> {
+        return ArrayList<MyGridLayoutScopeModel>().also {
+            scope.invoke(GridLayoutScope { weight, content ->
+                it.add(MyGridLayoutScopeModel(weight, content))
+            })
+        }
+    }
+
+    /**
+     * create by iso88591 at 2022/8/24
+     *
+     */
+//    @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3WindowSizeClassApi::class)
+    @Composable
+    private fun UseLayout(
+        spanCount: Int,
+        //横/竖 的间隔
+        vGap: Int = 0,
+        hGap: Int = 0,
+        modifier: Modifier = Modifier,
+        items: List<MyGridLayoutScopeModel>
+    ) {
+
+        Layout(measurePolicy = MeasurePolicy { measurables, parentConstraints ->
+
+
+            //计算宽度和高度 前记得要减去
+            //计算每一份的宽度
+            val perWidth =
+                (parentConstraints.maxWidth - (spanCount - 1) * hGap.dp.roundToPx()) / spanCount.toFloat()
+            //计算每一份的高度
+            val perHeight = perWidth
+
+            log("=======${perWidth}")
+
+            val places = measurables.mapIndexed { index, measurable ->
+
+                val scopeModel = items[index]
+                val thisWidth =
+                    (perWidth * scopeModel.weight + hGap.dp.roundToPx() * (scopeModel.weight - 1)).toInt()
+                val thisHeight = perHeight.toInt()
+                measurable.measure(
+                    parentConstraints.copy(
+                        minWidth = thisWidth,
+                        maxWidth = thisWidth,
+                        minHeight = thisHeight,
+                        maxHeight = thisHeight,
+                    )
+                )
+
+            }
+
+            layout(parentConstraints.maxWidth, parentConstraints.maxHeight) {
+
+                var start = 0
+                places.forEachIndexed { index, placeable ->
+                    val current = items[index]
+                    val hIndex = start % spanCount
+                    val vIndex = start / spanCount
+                    placeable.place(
+                        (perWidth * hIndex + hIndex * hGap.dp.roundToPx()).toInt(),
+                        (perHeight * vIndex + vIndex * vGap.dp.roundToPx()).toInt()
+                    )
+                    start += current.weight
+                }
+
+            }
+
+        }, content = {
+            items.forEach {
+                it.content()
+            }
+        }, modifier = modifier)
+
+
+//        LazyVerticalGrid(cells = GridCells.Adaptive(1000.dp), content = {
+//            itemsIndexed(items) { index, item ->
+//                Box(
+//                    modifier = Modifier
+//                        .width(perWidth * item.weight)
+//                        .height(perHeight * item.weight)
+//                ) {
+//                    item.content()
+//                }
+//            }
+//
+//        })
+    }
+
 
     @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3WindowSizeClassApi::class)
     @Composable
@@ -122,3 +279,42 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+
+//Layout(measurePolicy = MeasurePolicy { measurables, outConstraints ->
+//
+////            outConstraints.copy()
+//            log("约束", outConstraints.toString())
+//
+//            //1. 把所有子的可测量的（Measurable） 测量(传入约束)得到PlaceAble
+//            val places = measurables.mapIndexed { index, measurable ->
+//                measurable.measure(outConstraints.copy()).apply {
+//
+//                    log("遍历子并且测量", this.toString())
+//
+//                }
+//            }
+//
+//            layout(
+//                outConstraints.maxWidth,
+//                outConstraints.maxHeight
+//            ) {
+//
+//                //摆放子item的逻辑
+//                places.forEachIndexed { index, placeable ->
+//                    placeable.place(0, 0, 0f)
+//                    log("遍历子并摆放", placeable.toString())
+//                }
+//
+//            }
+//
+//        }, content = {
+//            remember {
+//                MyGridLayoutScopeImpl()
+//            }.also {
+//                it.content()
+////                    it.items.forEachIndexed { index, myGridLayoutScopeModel ->
+////
+////                    }
+//            }
+//        })
--- app/src/main/java/me/iso88591/cacu/logics/CacuKeys.kt
+++ app/src/main/java/me/iso88591/cacu/logics/CacuKeys.kt
@@ -1,13 +1,19 @@
 package me.iso88591.cacu.logics
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,69 +25,91 @@ import androidx.compose.ui.unit.sp
  * 在ui显示中一行占几个位置
  *
  */
+
+@Composable
+private fun Bg(
+    color: Color,
+    modifier: Modifier,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(1000.dp))
+            .background(color),
+        contentAlignment = Alignment.Center
+    ) {
+        content()
+    }
+}
+
+@Composable
+private fun SimpleText(
+    text: String,
+    fontSize: TextUnit = 30.sp,
+    color: Color = Color.White
+) {
+    Text(
+        text = text,
+        fontSize = fontSize,
+        color = color,
+        textAlign = TextAlign.Center,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
 sealed class CacuKeys(
     val simpleText: String = "",
-    val spanCount: Int = 1,
+    var spanCount: Int = 1,
+    val textColor: Color = Color.White,
+    val bgColor: Color = Color(0xff333333),
     val keyCode: Int = keyCodeGenerate++,
-//    val style: CacuButtonStyle = CacuButtonStyle(),
 ) {
 
     companion object {
-
         private var keyCodeGenerate = 0
+    }
+
+    @Composable
+    open fun Show(modifier: Modifier) {
+        Bg(
+            color = bgColor,
+            modifier = modifier
+        ) {
+            SimpleText(
+                text = simpleText,
+                color = textColor
+            )
+        }
+    }
+
+    open class ImageCacuKeys(
+        @DrawableRes private val imageResId: Int,
+        spanCount: Int = 1
+    ) : CacuKeys(
+        "",
+        spanCount
+    ) {
+
+        @Composable
+        override fun Show(modifier: Modifier) {
+            Box(
+                modifier = modifier,
+                contentAlignment = Alignment.Center
+            ) {
+                Image(painter = painterResource(id = imageResId), contentDescription = "")
+            }
+        }
 
     }
-//
-//    @Composable
-//    protected fun Bg(
-//        color: Color = Color(0xffA5A5A5),
-//        modifier: Modifier,
-//        content: @Composable () -> Unit
-//    ) {
-//        Box(
-//            modifier = modifier
-//                .background(color),
-//            contentAlignment = Alignment.Center
-//        ) {
-//            content()
-//        }
-//    }
-//
-//    @Composable
-//    protected fun SimpleText(
-//        text: String,
-//        fontSize: TextUnit = 30.sp,
-//        color: Color = Color.Black
-//    ) {
-//        Text(
-//            text = text,
-//            fontSize = fontSize,
-//            color = color,
-//        )
-//    }
-//
-//    @Composable
-//    open fun Show(modifier: Modifier) {
-//        Bg(modifier = modifier) {
-//            SimpleText(text = "1")
-//        }
-//    }
-//
-//    //text
-////    class Text(str: String) : CacuKeys(str)
-//
-//
-//    //归零键
-//    object C : CacuKeys(
-////        "C",
-////        CacuButtonStyle(
-////            bgColor = Color(0xffA5A5A5),
-////            textColor = Color(0xff030303),
-////        ),
-//        0
-//    )
-//
-//    //括号
+
+    //归零键
+    object C : CacuKeys(
+        "C",
+        textColor = Color.Black,
+        bgColor = Color(0xffA5A5A5)
+    )
+
+    //括号
 //    object Brackets : CacuKeys(
 ////        "( )",
 ////        CacuButtonStyle(
@@ -90,92 +118,70 @@ sealed class CacuKeys(
 ////        ),
 //        1
 //    )
-//
-//    //delete
-//    object Delete : CacuKeys(
-////        "Del",
-////        CacuButtonStyle(
-////            bgColor = Color(0xffA5A5A5),
-////            textColor = Color(0xff030303),
-////        ),
-//        2
-//    )
-//
-//    //+
-//    object Plus : CacuKeys(
-////        "+",
-////        CacuButtonStyle(
-////            bgColor = Color(0xffFEA00A),
-////            textColor = Color.White,
-////        ),
-//        3
-//    )
-//
-//    //-
-//    object Reduce : CacuKeys(
-////        "-",
-////        CacuButtonStyle(
-////            bgColor = Color(0xffFEA00A),
-////            textColor = Color.White,
-////        ),
-//        4
-//    )
-//
-//    //乘法
-//    object Mul : CacuKeys(
-////        "x",
-////        CacuButtonStyle(
-////            bgColor = Color(0xffFEA00A),
-////            textColor = Color.White,
-////        ),
-//        5
-//    )
-//
-//    //除法
-//    object Div : CacuKeys(
-////        "/",
-////        CacuButtonStyle(
-////            bgColor = Color(0xffFEA00A),
-////            textColor = Color.White,
-////        ),
-//        6
-//    )
-//
-//    //求结果
-//    object Result : CacuKeys(
-////        "=", CacuButtonStyle(
-////            bgColor = Color(0xffFEA00A),
-////            textColor = Color.White,
-////        ),
-//        7
-//    )
-//
-//    //百分号
-//    object Percent : CacuKeys(
-////        "%",
-////        CacuButtonStyle(
-////            bgColor = Color(0xffA5A5A5),
-////            textColor = Color(0xff030303),
-////        ),
-//        8
-//    )
-//
-//    object Point : CacuKeys(
-////        ".",
-////        CacuButtonStyle(
-////            bgColor = Color(0xff333333),
-////            textColor = Color.White,
-////        ),
-//        9
-//    )
-//
-//    //数字
-//    class Num(val number: Number) : CacuKeys(
-////        "${number}",
-////        CacuButtonStyle(
-////            bgColor = Color(0xff333333),
-////            textColor = Color.White,
-////        )
-//    )
+
+    //delete
+    object Delete : CacuKeys(
+        "Del",
+        textColor = Color.White,
+        bgColor = Color(0xffFEA00A)
+    )
+
+    //+
+    object Plus : CacuKeys(
+        "+",
+        textColor = Color.White,
+        bgColor = Color(0xffFEA00A)
+    )
+
+    //-
+    object Reduce : CacuKeys(
+        "-",
+        textColor = Color.White,
+        bgColor = Color(0xffFEA00A)
+    )
+
+    //+或者-
+    object PlusOrReduce:CacuKeys(
+        "+/-",
+        textColor = Color.Black,
+        bgColor = Color(0xffA5A5A5)
+    )
+
+    //乘法
+    object Mul : CacuKeys(
+        "x",
+        textColor = Color.White,
+        bgColor = Color(0xffFEA00A)
+    )
+
+    //除法
+    object Div : CacuKeys(
+        "/",
+        textColor = Color.White,
+        bgColor = Color(0xffFEA00A)
+    )
+
+    //求结果
+    object Result : CacuKeys(
+        "=",
+        textColor = Color.White,
+        bgColor = Color(0xffFEA00A)
+    )
+
+    //百分号
+    object Percent : CacuKeys(
+        "%",
+        textColor = Color.Black,
+        bgColor = Color(0xffA5A5A5)
+    )
+
+    object Point : CacuKeys(
+        ".",
+    )
+
+    //数字 01234567879
+    class Num(val number: Number, weight: Int = 1) : CacuKeys(
+        "${number}", weight
+    )
 
 }
