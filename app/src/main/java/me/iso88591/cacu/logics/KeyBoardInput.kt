eecb4b3ce44bca6d61a6d1f7c1c5b75f66bfdda4
iso88591 <18179157787@163.com>
2022-08-25 16:01:54 +0800

    逻辑完善中

--- app/src/main/java/me/iso88591/cacu/logics/KeyBoardInput.kt
+++ app/src/main/java/me/iso88591/cacu/logics/KeyBoardInput.kt
@@ -1,18 +1,20 @@
 package me.iso88591.cacu.logics
 
+import java.text.DecimalFormat
+import kotlin.math.pow
 import kotlin.math.roundToInt
 
 //计算逻辑
 class KeyBoardInput {
 
+    private var num1Str: String = ""
+    private var num2Str: String = ""
+
     //左边数字
     private var num1: Double = 0.0
 
-    private var num1HasPoint = false
-
     //右边数字
     var num2: Double? = null
-    private var num2HasPoint = false
 
     //根据操作id 来进行运算 后面可能会换成枚举来
     private val OP_NULL = CacuKeys.None
@@ -42,23 +44,36 @@ class KeyBoardInput {
             is CacuKeys.Num -> {
                 doWhenOp({
                     //没有操作符 新增符号到第一个数字上
-                    num1 = num1 * 10 + key.number.toDouble()
-                    show = num1.convertNum2DivideString()
+                    num1Str += (key.number.toInt())
+                    num1 = num1Str.toDoubleOrNull() ?: 0.0
+//                    num1 = if (num1HasPoint) {
+//                        //小数位数分之一   第0位 则乘 1/10 第1位 则乘 1/100
+//                        num1 + (key.number.toDouble() / (10.0.pow(num1PointAfterCount + 1)))
+//                    } else {
+//                        num1 * 10 + key.number.toDouble()
+//                    }
+                    show = num1Str
                 }, {
-                    num2 = (num2 ?: 0.0) * 10 + key.number.toDouble()
-                    show = (num2 ?: 0.0).convertNum2DivideString()
+                    num2Str += (key.number.toInt())
+                    num2 = num2Str.toDoubleOrNull()
+//                    num2 = if (num2HasPoint) {
+//                        (num2 ?: 0.0) + key.number.toDouble() / (10.0.pow(num2PointAfterCount + 1))
+//                    } else {
+//                        (num2 ?: 0.0) * 10 + key.number.toDouble()
+//                    }
+                    show = num2Str
                 })
             }
             CacuKeys.Point -> {
                 doWhenOp({
-                    num1HasPoint = true
                     //没有操作符 新增符号到第一个数字上
 //                    num1 = num1 * 10 + key.number.toDouble()
-                    show = num1.convertNum2DivideString() + "."
+                    num1Str += "."
+                    show = num1Str
                 }, {
-                    num2HasPoint = true
+                    num2Str += "."
 //                    num2 = num2 * 10 + key.number.toDouble()
-                    show = (num2 ?: 0.0).convertNum2DivideString() + "."
+                    show = num2Str
                 })
             }
 
@@ -67,8 +82,8 @@ class KeyBoardInput {
 
                 //更新操作符
                 if (num2 == null) {
-                    opId = key//TODO("赋值操作符")
-                    show = num1.convertNum2DivideString()
+                    opId = key
+                    show = num1Str
                 } else {
                     //有数字二 那么把上次结果算出来
                     show = cacuAndClear()
@@ -81,7 +96,7 @@ class KeyBoardInput {
                 //求结果
                 doWhenOp({
                     //都没有操作符 直接丢弃了
-                    show = num1.convertNum2DivideString()
+                    show = num1.trimEnd0()
                 }, {
                     //根据操作符来计算
                     show = cacuAndClear()
@@ -91,9 +106,34 @@ class KeyBoardInput {
             CacuKeys.C -> {
                 //清除所有操作
                 num1 = 0.0
+                num1Str = ""
                 num2 = null
+                num2Str = ""
                 opId = OP_NULL
                 show = emptyShow()
+            }
+            CacuKeys.Percent -> {
+                doWhenOp({
+                    num1 /= 100
+                    num1Str = num1.trimEnd0()
+                    show = num1Str
+                }, {
+                    num2 = (num2 ?: 0.0) / 100
+                    num2Str = (num2 ?: 0.0).trimEnd0()
+                    show = num2Str
+                })
+            }
+            CacuKeys.PlusOrReduce->{
+                //正负号
+                doWhenOp({
+                    num1 *= -1
+                    num1Str = num1.trimEnd0()
+                    show = num1Str
+                }, {
+                    num2 = (num2 ?: 0.0) * -1
+                    num2Str = (num2 ?: 0.0).trimEnd0()
+                    show = num2Str
+                })
             }
             else -> {}
         }
@@ -106,28 +146,36 @@ class KeyBoardInput {
         when (opId) {
             CacuKeys.Plus -> {
                 num1 += (num2 ?: 0.0)
+                num1Str = num1.trimEnd0()
                 num2 = null
+                num2Str = ""
                 opId = OP_NULL
                 opId = OP_NULL
-                show = num1.convertNum2DivideString()
+                show = num1Str
             }
             CacuKeys.Reduce -> {
                 num1 -= (num2 ?: 0.0)
+                num1Str = num1.trimEnd0()
                 num2 = null
+                num2Str = ""
                 opId = OP_NULL
-                show = num1.convertNum2DivideString()
+                show = num1Str
             }
             CacuKeys.Mul -> {
                 num1 *= (num2 ?: 0.0)
+                num1Str = num1.trimEnd0()
                 num2 = null
+                num2Str = ""
                 opId = OP_NULL
-                show = num1.convertNum2DivideString()
+                show = num1Str
             }
             CacuKeys.Div -> {
                 num1 /= (num2 ?: 0.0)
+                num1Str = num1.trimEnd0()
                 num2 = null
+                num2Str = ""
                 opId = OP_NULL
-                show = num1.convertNum2DivideString()
+                show = num1Str
             }
             else -> {
 
@@ -136,20 +184,7 @@ class KeyBoardInput {
         return show
     }
 
+    private val decimalFormat = DecimalFormat("###################.###########")
+    private fun Double.trimEnd0() = decimalFormat.format(this)
 
-}
-
-
-//TODO:转化当前数字 变成带 逗号隔开的数字 eg:123,456,789
-private fun Double.convertNum2DivideString(divideStr: String = ","): String {
-//    val valueStr = this.toString()
-    val roundToInt = this.roundToInt()
-    val log= if (this > roundToInt){
-        "有小数点"
-    }else{
-        "没有小数点"
-    }
-//
-//    val hasPoint = valueStr.contains(".")
-    return toString() + log
 }
